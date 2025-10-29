0052e38708a30c983d44a61a926b05809f058da9
KOI-SONN MANES MANA <104087468+Amemorte99@users.noreply.github.com>
2025-10-29 09:31:53 +0100

    redirection

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    reactStrictMode: false,
+    images: {
+      domains: ["localhost"],
+    },
+  };
+  
+
+  
+export default nextConfig;
